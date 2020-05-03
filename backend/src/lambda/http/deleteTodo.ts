87d821074277e1f1526f176047aa9d29a28a72bc
Jitendra Choudhary <jitendrachoudhary@Jitendras-MacBook-Pro.local>
2020-05-03 14:50:42 +0530

    backend-serverless

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,54 @@
 import 'source-map-support/register'
+import * as AWS from 'aws-sdk'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { parseUserId } from '../../auth/utils'
+
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const toDoTable = process.env.TODO_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
-  // TODO: Remove a TODO item by id
-  return undefined
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+  const userId = parseUserId(jwtToken)
+
+  const itemToUpdate = await docClient.get({
+      TableName: toDoTable,
+      Key: {
+          todoId: todoId,
+          userId: userId
+      }
+  }).promise()
+
+  if (!itemToUpdate.Item){
+    return {
+        statusCode: 404,
+        body: JSON.stringify({
+            error: 'TODO item does not exist'
+        })
+    }
+  }
+
+  await docClient.delete({
+      TableName: toDoTable,
+      Key: {
+          todoId: todoId,
+          userId: userId
+      }
+  }).promise()
+  
+  return {
+      statusCode: 204,
+      headers: {
+          'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+          info: 'item deleted succesfully'
+      })
+  }
+
 }
--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,35 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { getUserId } from '../utils'
+import { updateToDo } from '../logicLayer/todo'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('updating TODO item')
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  const userId = getUserId(event)
+
+  const response = await updateToDo( userId, todoId, updatedTodo)
+  if (response == null)
+      return {
+        statusCode: 404,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+          error: 'TODO item does not exist'
+        })
+    }
+
+  return {
+      statusCode: 204,
+      headers: {
+          'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        updatedTodo
+      })
+  }
 }
