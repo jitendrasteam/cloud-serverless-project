0661db312de2844bde8e973812735782fe3edb45
Jitendra Choudhary <jitendrachoudhary@Jitendras-MacBook-Pro.local>
2020-05-03 17:43:35 +0530

    ui and certificate added

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -10,11 +10,28 @@ import { createLogger } from '../../utils/logger'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
-
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const cert = ''
+const cert = `-----BEGIN CERTIFICATE-----
+MIIDBzCCAe+gAwIBAgIJASJUu+Lh96JpMA0GCSqGSIb3DQEBCwUAMCExHzAdBgNV
+BAMTFmRldi16Ym1qYTNicy5hdXRoMC5jb20wHhcNMjAwNTAzMTEzMzQ5WhcNMzQw
+MTEwMTEzMzQ5WjAhMR8wHQYDVQQDExZkZXYtemJtamEzYnMuYXV0aDAuY29tMIIB
+IjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyy1Muum45EtVGrgKoNVHWGEP
+s/Bs0mry5tCVMk5NyNs9Rlb3bIAabweWSi4rJUHqMouN0PlRlfxyWBVxifxB9NbX
+pi7Tz/P8ciS3MRBcE5QDi2OgYXJzTWdGghoU3DVBR0hC8MbceuZl5NIWycnuf2he
+By5MxZXfWVxX8pOoX0iTVLoVms8kdmB+MbAC5rA//3Z0UjY8jZOK9Yez1buaF5rN
+hLWCrCxUNju2bqeOvRqbxbMElzoa2M4BJvQQz/rtRK7J9Bynx71ksznGS2hy0K91
+6jygvcSjPJ7y6R12VBZAi3DrmL0zjjHnSzUgyFnHS2Y6LqgQb1YfgpE6ATNs+QID
+AQABo0IwQDAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBT+6IfbxNuusp94lyia
+hB+mjIn+xDAOBgNVHQ8BAf8EBAMCAoQwDQYJKoZIhvcNAQELBQADggEBAETVRX4A
+fQJ9Ji0LT/sCMNGr67NUHPKmIPfHeAdu1eshOVyS/QJTznLkGYGsZhU+1dHtuw+n
+mJFuttHbRoM73AO+g7LVf9BwoLr3Mfs3uI/wHe5DRzzDW+lUVDJYkDXmNROc1pw7
+HmN/pOfy7uE74YOoYvHmIP8XrLc7kxM3uubLPwCLoD7T7JMO2WxGOoXX0hNaaSyS
+T7QwvjvtIYVujgYfgeig+jWFnejaBVi6dLTcBeNcslEDmNAZWAVDTYCy7aQUwJTa
+ONMmfDWHjZKCMbwGzuj6bG9385JoSki47YmRIUVYn41AorWN5BPocYLZ6w95TTgd
+9SWdvnHV4BEtlZM=
+-----END CERTIFICATE-----`
 //const jwksUrl = '...'
 
 export const handler = async (
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'q7fiuyxzb0'
+export const apiEndpoint = `https://${apiId}.execute-api.ap-south-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-zbmja3bs.auth0.com',            // Auth0 domain
+  clientId: 'Ime7FDMyccgJqG0f2hUCvL0H90z4lUD7',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
